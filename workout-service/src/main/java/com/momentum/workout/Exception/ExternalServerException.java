444e55a8c1bd5393c2dea7d83c70d02b1aba5b4e
Melina Skytén <melina.skyten@gmail.com>
2026-04-08 14:51:24 +0200

    Improved error handling with GlobalExceptionHandler class

--- workout-service/src/main/java/com/momentum/workout/Exception/ExternalServerException.java
+++ workout-service/src/main/java/com/momentum/workout/Exception/ExternalServerException.java
@@ -1,6 +1,5 @@
 package com.momentum.workout.Exception;
 
-//5xx
 public class ExternalServerException extends RuntimeException {
     public ExternalServerException(String message) {
         super(message);
