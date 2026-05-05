62d8ae88a54e294ba3b1770cd6801fb549f81b84
Melina Skytén <melina.skyten@gmail.com>
2026-05-05 10:32:14 +0200

    Fetching user and stats data for frontend

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const AUTH_URL = 'http://localhost:8080/auth'
 
+const getToken = () => localStorage.getItem('token')
+
 export const login = async (email, password) => {
     const response = await axios.post(`${AUTH_URL}/login`, { email, password })
     return response.data
@@ -9,5 +11,13 @@ export const login = async (email, password) => {
 
 export const register = async (email, password) => {
     const response = await axios.post(`${AUTH_URL}/register`, { email, password })
+    return response.data
+}
+
+export const getUserDetails = async() => {
+    const response =
+        await axios.get(`${AUTH_URL}/me`,
+            {headers: { Authorization: `Bearer ${getToken()}` }
+            })
     return response.data
 }
