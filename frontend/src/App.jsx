d9b43895c7bdb777b174c3f82ae85607503c71be
Melina Skytén <melina.skyten@gmail.com>
2026-05-13 11:18:40 +0200

    redirected all endpoints to login page if user isnt logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useLocation, Navigate} from 'react-router-dom'
+import {useAuth} from "./context/AuthContext.jsx";
 import LoginPage from './pages/LoginPage.jsx'
 import WorkoutsPage from './pages/WorkoutsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
@@ -6,6 +7,12 @@ import ViewWorkoutPage from "./pages/ViewWorkoutPage.jsx"
 import NavBar from "./components/NavBar.jsx";
 import CreateWorkoutPage from "./pages/CreateWorkoutPage.jsx"
 import './App.css'
+
+function ProtectedRoute ({children}) {
+    const {token} = useAuth()
+    if (!token) return <Navigate to="/login"/>
+    return children
+}
 
 function Layout() {
     const location = useLocation()
@@ -17,10 +24,10 @@ function Layout() {
             <div className={isAuthPage ? "auth-content" : "main-content"}>
                 <Routes>
                     <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/workouts" element={<WorkoutsPage/>}/>
-                    <Route path="/me" element={<ProfilePage/>}/>
-                    <Route path="/workouts/:id" element={<ViewWorkoutPage/>}/>
-                    <Route path="/workouts/new" element={<CreateWorkoutPage/>}/>
+                    <Route path="/workouts" element={<ProtectedRoute><WorkoutsPage/></ProtectedRoute>}/>
+                    <Route path="/me" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>}/>
+                    <Route path="/workouts/:id" element={<ProtectedRoute><ViewWorkoutPage/></ProtectedRoute>}/>
+                    <Route path="/workouts/new" element={<ProtectedRoute><CreateWorkoutPage/></ProtectedRoute>}/>
                 </Routes>
             </div>
         </div>
